ccbf07129eba7421d5575f8c67bfd18399c129ba
Eneb <135270219+ametrahs@users.noreply.github.com>
2025-03-04 07:16:31 -0800

    adding backeDepdurl

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-ab3ed/us-central1/api",
+  // baseURL: "http://127.0.0.1:5001/clone-ab3ed/us-central1/api",
+  baseURL: " https://amazon-api-deploy-afq6.onrender.com",
 });
 
 export { axiosInstance };
